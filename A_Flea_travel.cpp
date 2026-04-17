7a130f22114e2d4830f1f6b6a3fe358c5ef05616
Harendra Kumar <harendrarajput9627@gmail.com>
2026-04-17 18:51:49 +0530

    Add files via upload

---
+++ A_Flea_travel.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+#include<numeric>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define f first
+#define s second
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define cin(v) for(auto &x : v)cin >> x
+#define cin_2D(v) for(auto &row : v) for(auto &x : row)cin>>x
+#define cout(v) for(auto& x : v)cout << x << ' '
+#define cout_2D(v) for(auto& row : v) for(auto x : row) cout << x << ' '; cout << endl
+#define sz(v) (int)(v.size())
+#define vi vector<ll>
+#define vvi vector<vi>
+#define vec_max(v) *max_element(all(v))
+#define vec_min(v) *min_element(all(v))
+#define lb(v, x) distance((v).begin(), lower_bound(all(v), (x)))
+#define ub(v, x) distance((v).begin(), upper_bound(all(v), (x)))
+#define unique(v) sort(all(v)), v.erase(unique(all(v)), v.end()), v.shrink_to_fit()
+#define Y cout<<"YES"
+#define N cout<<"NO"
+
+#define for0(i, a) for (ll i = 0; i < (a); ++i)
+#define for1(i, a, b) for (ll i = a; i < (b); ++i)
+#define for2(i, a, b, c) for (ll i = a; i < (b); i += (c))
+#define for0_r(i, a) for (ll i = (a)-1; i >= (0); --i)
+#define for1_r(i, a, b) for (ll i = (b)-1; i >= (a); --i)
+#define for2_r(i, a, b, c) for (ll i = b; i < (a); i -= (c))
+
+string decToBinary(ll n) {
+    if (n == 0) return "0";
+    string binary = "";
+    while (n > 0) {
+        binary = char('0' + (n % 2)) + binary;
+        n /= 2;
+    }
+    return binary;
+}
+
+int max2D(const vector<vector<int>>& v) {
+    int mx = INT_MIN;
+    for (auto &row : v)
+        mx = max(mx, *max_element(all(row)));
+    return mx;
+}
+int min2D(const vector<vector<int>>& v) {
+    int mn = INT_MAX;
+    for (auto &row : v)
+        mn = min(mn, *min_element(all(row)));
+    return mn;
+}
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+void solve(){
+    ll n;
+    cin >> n;
+    for0(i,10){
+        if(pow(2,i) == n) {Y;return;}
+    }
+    N;
+}
+
+int main(){
+    int t = 1;
+    // cin>>t;
+    while(t--){
+        solve();
+        cout<<endl;
+    }
+}
+
+//     //    ///    ///////   ///////// //     // //////    ///////      ///  
+//     //  //   //  //     // //        ///    // //    //  //     //  //   // 
+//     // //     // //     // //        ////   // //     // //     // //     //
+///////// ///////// ////////  ///////   // /// // //     // ////////  /////////
+//     // //     // // //     //        //   //// //     // // //     //     //
+//     // //     // //   //   //        //    /// //    //  //   //   //     //
+//     // //     // //     // ///////// //     // //////    //     // //     //
